7fdd9e6b9c195d53cfcf5ca931ab16c54bdba6de
Stephanie Forlemu <stdjoso@email.unc.edu>
2022-08-28 14:27:43 -0400

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,43 @@
+"""EX01 - Chardle - A cute step toward Wordle"""
+
+__author__ = "730390832"
+
+five_char_word: str = input("Enter a 5-character word: ")
+if len(five_char_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+single_char: str = input("Enter a single character: ")
+if len(single_char) != 1: 
+    print("Error: Character must be a single character.")
+    exit()
+print("Searching for " + single_char + " in " + five_char_word)
+
+if single_char == five_char_word[0]:
+    print(single_char + " found at index 0")
+if single_char == five_char_word[1]:
+    print(single_char + " found at index 1")
+if single_char == five_char_word[2]:
+    print(single_char + " found at index 2")
+if single_char == five_char_word[3]:
+    print(single_char + " found at index 3")
+if single_char == five_char_word[4]:
+    print(single_char + " found at index 4")
+
+count: int = 0
+if single_char == five_char_word[0]:
+    count += 1
+if single_char == five_char_word[1]:
+    count += 1
+if single_char == five_char_word[2]:
+    count += 1
+if single_char == five_char_word[3]:
+    count += 1
+if single_char == five_char_word[4]:
+    count += 1
+if count == 1:
+    print(str(count) + " intance of " + single_char + " found in " + five_char_word)
+else:
+    if count > 1:
+        print(str(count) + " intances of " + single_char + " found in " + five_char_word)
+    if count == 0:
+        print("No intances of " + single_char + " found in " + five_char_word)
